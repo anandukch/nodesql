0ca24aaddecfb9c151ec06e5a6d6ea6af3be830d
anandukch <anandukch@gmail.com>
2021-11-19 15:25:14 +0530

    db updated

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const path=require('path');
 const hbs=require('express-handlebars');
 const bodyParser = require("body-parser");
 const sequelize = require("./utils/db");
+const cookieParser=require('cookie-parser')
+const fileUpload=require('express-fileupload')
 const User = require("./models/user");
-
+const session = require('express-session')
+app.use(session({secret:'Key',cookie:{maxAge:60000}}))
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'hbs');
 // app.engine('hbs', hbs({extname:'hbs', defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials'}))
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload())
 sequelize
   .authenticate()
   .then(function (err) {
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,26 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const Sequelize = require("sequelize");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+function auth(req, res, next) {
+  const token = req.headers.authorization.split(" ")[1];
+  decodedData = jwt.verify(token, "test");
+  User.findOne({
+    where: {
+      user_email: req.body.user_email,
+      user_password: req.body.user_password,
+    },
+  })
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch(() => {
+      return res.json("Unauthorised");
+    });
+}
 
 router.get("/", (_, res) => {
   User.findAll({
@@ -14,32 +34,36 @@ router.get("/", (_, res) => {
       res.send(err);
     });
 });
-router.get('/login',(req,res)=>{
-  res.render('login');
-})
-router.post('/login',(req,res)=>{
-  console.log(req.body);
+
+router.post("/login", async (req, res) => {
+  let { user_email, user_password } = req.body;
   User.findOne({
-    where:{
-      user_email:req.body.user_email,
-      user_password:req.body.user_password
-    }
-  }).then((user)=>{
-    if(user){
-      res.send(user);
-    }
-    else{
-      res.send('User not found');
-    }
-  })
-})
+    where: {
+      user_email: user_email,
+    },
+  }).then((data) => {
+    let user=data.dataValues;
+    let isValid = bcrypt.compareSync(user_password, user.user_password);
+    if (!isValid) res.json("incorrect password");
+
+    const token = jwt.sign({ id: user.user_id }, "test", {
+      expiresIn: "1h",
+    });
+    res.json({ ...user, token });
+  }).catch(()=>{
+    res.json("user not found")
+  });
+});
+
 router.get("/insert", (_, res) => {
   res.render("insert");
 });
-router.post("/insert", (req, res) => {
+
+router.post("/insert", async (req, res) => {
   let { user_name, user_email, user_password, total_orders } = req.body;
+  user_password = await bcrypt.hash(user_password, 10);
+  let image = req.files.user_image;
   total_orders = parseInt(total_orders);
-  console.log(req.body);
   User.create({
     user_name: user_name,
     user_email: user_email,
@@ -49,6 +73,14 @@ router.post("/insert", (req, res) => {
     last_logged_in: new Date(),
   })
     .then((user) => {
+      image.mv("./public/images/" + user.user_id + ".jpg", (err, done) => {
+        if (!err) {
+          console.log("uploaded");
+        } else {
+          console.log(err);
+        }
+      });
+
       res.send(user);
     })
     .catch((err) => {
@@ -56,6 +88,7 @@ router.post("/insert", (req, res) => {
       console.log(err);
     });
 });
+
 router.get("/details/:id", (req, res) => {
   User.findOne({
     where: {
@@ -83,30 +116,23 @@ router.delete("/delete/:id", (req, res) => {
       res.send("error" + err);
     });
 });
-router.put("/update/:id", (req, res) => {
+
+router.put("/update/:id", auth, (req, res) => {
   User.update(req.body, {
     where: {
       user_id: req.params.id,
     },
   })
     .then((user) => {
+      console.log(user);
       res.json(user);
     })
     .catch((err) => {
       res.send("error" + err);
     });
 });
+
 router.get("/image/:id", (req, res) => {
-  User.findOne({
-    where: {
-      user_id: req.params.id,
-    },
-  })
-    .then((user) => {
-      res.json(user);
-    })
-    .catch((err) => {
-      res.send("error" + err);
-    });
+  res.render('image',{id:req.params.id})
 });
 module.exports = router;
